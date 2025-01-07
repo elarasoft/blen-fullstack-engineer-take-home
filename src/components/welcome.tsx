a664118947999daca5f8b4df8aa4edc4450ead01
Elarasoft <elarasoft.dev@gmail.com>
2025-01-07 06:30:57 -0800

    Add shadcn/ui style

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,9 +1,16 @@
+import { Button } from '@/components/ui/button';
 
 export default function Welcome() {
   return (
     <div className="flex flex-col items-center justify-center">
+      <div>
+        <Button>Click me</Button>
+      </div>
       <h1 className="text-2xl font-bold"> Welcome to Full Stack Engineer Assessment</h1>
-      <p className="text-sm text-gray-500">Follow the instructions in the <span className="text-muted-foreground font-bold">README.md</span> file</p>
+      <p className="text-sm text-gray-500">
+        Follow the instructions in the{' '}
+        <span className="font-bold text-muted-foreground">README.md</span> file
+      </p>
     </div>
-  )
+  );
 }
